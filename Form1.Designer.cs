17c2878020c7933a5d9d7e83c3de75887e781c58
Marye10 <marianaecpe10@gmail.com>
2023-10-20 12:31:24 -0500

    soidjawiuf

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,6 +29,11 @@
         private void InitializeComponent()
         {
             components = new System.ComponentModel.Container();
+            /*
+            ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            */
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             b_inicio = new Button();
             pictureBox1 = new PictureBox();
@@ -46,9 +51,19 @@
             timer10 = new System.Windows.Forms.Timer(components);
             Tiempo2 = new System.Windows.Forms.Timer(components);
             Tiempo3 = new System.Windows.Forms.Timer(components);
+            label1 = new Label();
+            Elementos_Filas = new TextBox();
+            Maquina = new DataGridView();
+            Carretilla1 = new DataGridViewTextBoxColumn();
+            Carretilla2 = new DataGridViewTextBoxColumn();
+            Carretilla3 = new DataGridViewTextBoxColumn();
+            button1 = new Button();
+            //chart1 = new Chart();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)Maquina).BeginInit();
+            //((System.ComponentModel.ISupportInitialize)chart1).BeginInit();
             SuspendLayout();
             // 
             // b_inicio
@@ -56,9 +71,10 @@
             b_inicio.BackColor = SystemColors.ControlLightLight;
             b_inicio.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point);
             b_inicio.ForeColor = SystemColors.ActiveBorder;
-            b_inicio.Location = new Point(599, 181);
+            b_inicio.Location = new Point(524, 213);
+            b_inicio.Margin = new Padding(3, 2, 3, 2);
             b_inicio.Name = "b_inicio";
-            b_inicio.Size = new Size(163, 39);
+            b_inicio.Size = new Size(143, 29);
             b_inicio.TabIndex = 0;
             b_inicio.Text = "Iniciar";
             b_inicio.UseVisualStyleBackColor = false;
@@ -67,9 +83,10 @@
             // pictureBox1
             // 
             pictureBox1.Image = Properties.Resources.Trebol;
-            pictureBox1.Location = new Point(401, 12);
+            pictureBox1.Location = new Point(351, 9);
+            pictureBox1.Margin = new Padding(3, 2, 3, 2);
             pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(163, 149);
+            pictureBox1.Size = new Size(143, 112);
             pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
             pictureBox1.TabIndex = 1;
             pictureBox1.TabStop = false;
@@ -83,9 +100,10 @@
             // pictureBox2
             // 
             pictureBox2.Image = Properties.Resources.Trebol;
-            pictureBox2.Location = new Point(599, 12);
+            pictureBox2.Location = new Point(524, 9);
+            pictureBox2.Margin = new Padding(3, 2, 3, 2);
             pictureBox2.Name = "pictureBox2";
-            pictureBox2.Size = new Size(163, 149);
+            pictureBox2.Size = new Size(143, 112);
             pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
             pictureBox2.TabIndex = 2;
             pictureBox2.TabStop = false;
@@ -94,9 +112,10 @@
             // pictureBox3
             // 
             pictureBox3.Image = Properties.Resources.Trebol;
-            pictureBox3.Location = new Point(794, 12);
+            pictureBox3.Location = new Point(695, 9);
+            pictureBox3.Margin = new Padding(3, 2, 3, 2);
             pictureBox3.Name = "pictureBox3";
-            pictureBox3.Size = new Size(163, 149);
+            pictureBox3.Size = new Size(143, 112);
             pictureBox3.SizeMode = PictureBoxSizeMode.Zoom;
             pictureBox3.TabIndex = 3;
             pictureBox3.TabStop = false;
@@ -112,22 +131,103 @@
             Tiempo3.Interval = 1000;
             Tiempo3.Tick += Tiempo3_Tick;
             // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(497, 154);
+            label1.Name = "label1";
+            label1.Size = new Size(187, 15);
+            label1.TabIndex = 4;
+            label1.Text = "Ingrese cuantos elementos quiere.";
+            label1.Click += label1_Click;
+            // 
+            // Elementos_Filas
+            // 
+            Elementos_Filas.Location = new Point(524, 172);
+            Elementos_Filas.Name = "Elementos_Filas";
+            Elementos_Filas.Size = new Size(138, 23);
+            Elementos_Filas.TabIndex = 5;
+            // 
+            // Maquina
+            // 
+            Maquina.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            Maquina.Columns.AddRange(new DataGridViewColumn[] { Carretilla1, Carretilla2, Carretilla3 });
+            Maquina.Location = new Point(967, 131);
+            Maquina.Name = "Maquina";
+            Maquina.RowTemplate.Height = 25;
+            Maquina.Size = new Size(17, 21);
+            Maquina.TabIndex = 6;
+            Maquina.Visible = false;
+            // 
+            // Carretilla1
+            // 
+            Carretilla1.HeaderText = "Carretilla1";
+            Carretilla1.Name = "Carretilla1";
+            // 
+            // Carretilla2
+            // 
+            Carretilla2.HeaderText = "Carretilla2";
+            Carretilla2.Name = "Carretilla2";
+            // 
+            // Carretilla3
+            // 
+            Carretilla3.HeaderText = "Carretilla3";
+            Carretilla3.Name = "Carretilla3";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(524, 260);
+            button1.Name = "button1";
+            button1.Size = new Size(143, 23);
+            button1.TabIndex = 7;
+            button1.Text = "Graficar_Funcion";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // chart1
+            // 
+            /*
+            chartArea1.Name = "ChartArea1";
+            chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            chart1.Legends.Add(legend1);
+            chart1.Location = new Point(673, 182);
+            chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            chart1.Series.Add(series1);
+            chart1.Size = new Size(300, 224);
+            chart1.TabIndex = 8;
+            chart1.Text = "chart1";
+            chart1.Click += this.chart1_Click;
+            */
+            // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
-            ClientSize = new Size(1193, 558);
+            ClientSize = new Size(1044, 418);
+           // Controls.Add(chart1);
+            Controls.Add(button1);
+            Controls.Add(Maquina);
+            Controls.Add(Elementos_Filas);
+            Controls.Add(label1);
             Controls.Add(pictureBox3);
             Controls.Add(pictureBox2);
             Controls.Add(pictureBox1);
             Controls.Add(b_inicio);
+            Margin = new Padding(3, 2, 3, 2);
             Name = "Form1";
             Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)Maquina).EndInit();
+            //((System.ComponentModel.ISupportInitialize)chart1).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
@@ -148,5 +248,13 @@
         private System.Windows.Forms.Timer timer10;
         private System.Windows.Forms.Timer Tiempo2;
         private System.Windows.Forms.Timer Tiempo3;
+        private Label label1;
+        private TextBox Elementos_Filas;
+        private DataGridView Maquina;
+        private DataGridViewTextBoxColumn Carretilla1;
+        private DataGridViewTextBoxColumn Carretilla2;
+        private DataGridViewTextBoxColumn Carretilla3;
+        private Button button1;
+        private System.CodeDom.CodeTypeReference chart1;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -2,6 +2,8 @@ namespace Procesos_estocasticos
 {
     public partial class Form1 : Form
     {
+        static float resultado_probabilidad, division;
+        static int valores_repetidos = 0;
         int t = 0, t2 = 0, t3 = 0;
         public Form1()
         {
@@ -13,7 +15,53 @@ namespace Procesos_estocasticos
             Tiempo1.Enabled = true;
             Tiempo2.Enabled = true;
             Tiempo3.Enabled = true;
-            b_inicio.Visible = false;
+            //b_inicio.Visible = false;
+            int elementos_fila;
+            Random aleat = new Random();
+
+            elementos_fila = int.Parse(Elementos_Filas.Text);
+
+            if (elementos_fila <= 5)
+            {
+                MessageBox.Show("Porfavor ingrese un valor mayor o igual a 5", "Error en elementos de Carretilla",
+                MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            else
+            {
+                    for (int j = 0; j < elementos_fila; j++)
+                    {
+                        Maquina.Rows.Add(aleat.Next(1, elementos_fila), aleat.Next(1, elementos_fila), aleat.Next(1, elementos_fila));
+                    }
+                
+                    division = 1f / elementos_fila; //Frecuencia relativa
+                    resultado_probabilidad = (float)Math.Pow(division, 3); //Resultado
+                    MessageBox.Show(resultado_probabilidad.ToString(), "El resultado de la probabilidad es",
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+               
+                    for (int j = 0; j < 2; j++)
+                    {
+                        for (int i = 0; i < elementos_fila; i++)
+                        {
+                            for (int l = j + 1; l < 2; l++)
+                            {
+                                for (int k = 0; k < elementos_fila; k++)
+                                {
+                                    if (Maquina.Rows[i].Cells[j].Value != null && Maquina.Rows[k].Cells[l].Value != null &&
+                                        Maquina.Rows[i].Cells[j].Value.Equals(Maquina.Rows[k].Cells[l].Value))
+                                    {
+                                        /*MessageBox.Show("Se encontró un valor repetido en la columna " + (j + 1) + " y columna " + (l + 1) + ": " + Maquina.Rows[i].Cells[j].Value, "Resultado",
+                                                        MessageBoxButtons.OK, MessageBoxIcon.Information);*/
+                                        valores_repetidos++;
+                                       // MessageBox.Show("Los valores repetidos: " + valores_repetidos.ToString(), )
+                                    }
+                                }
+                            }
+                        }
+                    }
+                
+            }
         }
 
         private void timer1_Tick(object sender, EventArgs e)
@@ -76,6 +124,25 @@ namespace Procesos_estocasticos
         {
             if (t == t2 && t2 == t3) MessageBox.Show("Ganaste");
             else MessageBox.Show("Perdiste");
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            float valor, porcentaje;
+            valor = 1 * (resultado_probabilidad) + 2 * (resultado_probabilidad) + 3 * (resultado_probabilidad); //Distribución masa de probabilidad
+            porcentaje = valor * 100;
+            MessageBox.Show(porcentaje.ToString(), "Promedio",
+                MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void chart1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
